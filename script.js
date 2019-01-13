2e9cebd7bb153304b125c94f0ece4dceecfca852
seeker <wildetangeant@gmail.com>
2019-01-13 14:26:29 -0800

    Got the fisher-yates scrambler in

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-grabquerent(querent) {
-  var querent = document.getElementById("querent").value;
-};
+
+function return_response() {
+var responses = ["cheesepuffs", "without dignity", "that was innapropriate", "I need to call the police now", "time is a flat circle", "only time will tell", "yes", "no"]
+
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+responses = shuffle(responses)
+console.log(responses)
+
+}
+
+
+
+
+// function grabQuerent(querent) {
+//   var querent = document.getElementById("querent").value;
+//   console.log("querent in grabQuerent is "+ querent)
+// }
